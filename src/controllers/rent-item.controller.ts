981df90a304ab0e8b0b6379114459ac861a10786
kristinavd <kristina.vidakovic403@gmail.com>
2023-07-13 18:09:44 +0200

    Implemented POST /rent_item

--- src/controllers/rent-item.controller.ts
+++ src/controllers/rent-item.controller.ts
@@ -1,4 +1,7 @@
 import { Request, Response } from "express";
+import { Book } from "../models/book.model";
+import { Rent } from "../models/rent.model";
+import { RentItem, RentItemInput } from "../models/rent-item.model";
 
 /**
  * @route POST /rent_item
@@ -13,6 +16,45 @@ const createRentItem = async(req: Request, res: Response) => {
     if (!rent || !book || !quantity) {
         return res.status(400).json({ message: "Missing values" });
     }
+
+    const b = await Book.findById(book);
+    if (!b || b.deleted) {
+        return res.status(400).json({ message: "Provided book ID doesn't exist" });
+    }
+
+    const r = await Rent.findById(rent);
+    if (!r || r.deleted) {
+        return res.status(400).json({ message: "Provided rent ID doesn't exist" });
+    }
+
+    const rentItem = await RentItem.findOne({
+        rent: r._id,
+        book: b,
+        deleted: false
+    });
+    if (rentItem) {
+        return res.status(400).json({ message: "Provided rent item alreay exist" });
+    }
+
+    try {
+        const rentItemInput : RentItemInput = {
+            quantity,
+            rent,
+            book: b,
+            deleted: false
+        };
+
+        const rentItemCreated = await RentItem.create(rentItemInput);
+
+        r.items.push(rentItemCreated);
+        await Rent.findByIdAndUpdate({ _id: r._id }, { $set: { items: r.items }, $inc: { __v: 1 } }, { new: true });
+
+        return res.status(201).json({ rentItem: rentItemCreated });
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+
 };
 
 export { createRentItem };
